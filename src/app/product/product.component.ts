efecfd22500ab46a4d27df293774822d18837f21
sahkirteke <sahhusamettin@windowslive.com>
2020-12-17 15:13:39 +0300

    denem

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  template: `<p>{{name}}</p>`,
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent implements OnInit {
+  constructor() {}
+  name = 'laptoggggggp';
+  ngOnInit(): void {}
+}
